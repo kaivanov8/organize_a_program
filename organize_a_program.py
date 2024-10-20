2823ab988eb9ba676a866eb7970b72f5a3abe574
kaivanov8 <kosiv6799@gmail.com>
2024-10-20 15:53:34 +0700

    Add files via upload

---
+++ organize_a_program.py
@@ -0,0 +1,9 @@
+# организация программ и методы строк
+my_string = input ("Введите текст:")
+a=len(my_string)
+print(a)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[a-1])
